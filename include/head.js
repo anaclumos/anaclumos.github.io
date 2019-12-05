beb076ca62e81298d5e589309b60db4e431200db
Sunghyun Cho <mail@chosunghyun.com>
2019-12-05 22:37:26 +0900

    Add Google Analytics

--- include/head.js
+++ include/head.js
@@ -1,1 +1,12 @@
-document.write("<script src=\"https://kit.fontawesome.com/83991c6240.js\" crossorigin=\"anonymous\"></script>");
+document.write(`
+<script src=\"https://kit.fontawesome.com/83991c6240.js\" crossorigin=\"anonymous\"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127955862-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-127955862-1');
+</script>
+`);
