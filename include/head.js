aeb619b4d739798c593a4cf4b706afae006847a7
Sunghyun Cho <mail@chosunghyun.com>
2019-12-24 23:23:11 +0900

    Remove Icons Support

--- include/head.js
+++ include/head.js
@@ -1,5 +1,4 @@
 document.write(`
-<script src=\"https://kit.fontawesome.com/83991c6240.js\" crossorigin=\"anonymous\"></script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127955862-1"></script>
 <script>
